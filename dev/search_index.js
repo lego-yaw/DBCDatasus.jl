82cc51291a4ec34ad03d6a994bfc0179d2df97e3
Documenter.jl <documenter@juliadocs.github.io>
2024-09-02 20:06:22 +0000

    build based on 00ea4e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReadDBC","category":"page"},{"location":"#ReadDBC","page":"Home","title":"ReadDBC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReadDBC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReadDBC]","category":"page"}]
+}
