e6bf09019f1943c6718775e3ae700f69b79402ab
Documenter.jl <documenter@juliadocs.github.io>
2026-09-17 19:52:07 +0000

    build based on b3cbe2f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DBCDatasus","page":"Home","title":"DBCDatasus","text":"Documentation for DBCDatasus.\n\n","category":"section"}]
+[{"category":"section","location":"#DBCDatasus","page":"Home","text":"Documentation for DBCDatasus.\n\n","title":"DBCDatasus"}]
 }
